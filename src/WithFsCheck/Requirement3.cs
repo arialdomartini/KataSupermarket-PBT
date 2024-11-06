8a909c9c7ee506013285eb26290c54d97e8ab553
Arialdo Martini <arialdo.martini@gmail.com>
2024-11-06 14:08:39 +0100

    More tests for Requirement 3

--- src/WithFsCheck/Requirement3.cs
+++ src/WithFsCheck/Requirement3.cs
@@ -1,7 +1,16 @@
+using FsCheck;
+using FsCheck.Xunit;
+
 namespace WithFsCheck;
+
+internal record Promo(Product Product, int Threshold, int Discount);
 
 public class Requirement3
 {
+    public Requirement3()
+    {
+        Arb.Register<ProductGenerator>();
+    }
     // Time to implement offers!
     //
     // As a cashier, 
@@ -13,5 +22,63 @@ public class Requirement3
     // * When I checkout 3 apples, the system charges 130 cents instead of 150
     // * When I checkout 2 pears, the system charges 45 cents instead of 60
     // * When I checkout 2 pineapples, the system charges 440 cents, as there are no offers for pineapples
+
+    [Fact]
+    void when_I_checkout_3_apples_the_system_charges_150_cents()
+    {
+        var promotion = new Promotion("apple", 3, 20);
+        var cart = new Cart(promotion);
+        
+        cart.Scan("apple");
+        cart.Scan("apple");
+        cart.Scan("apple");
+
+        var total = cart.Checkout();
+        
+        Assert.Equal(130, total);
+    }
+
+    [Fact]
+    void when_I_checkout_2_pears_the_system_charges_45_cents_instead_of_60()
+    {
+        var promotion = new Promotion("pear", 2, 15);
+        var cart = new Cart(promotion);
+        
+        cart.Scan("pear");
+        cart.Scan("pear");
+
+        var total = cart.Checkout();
+        
+        Assert.Equal(45, total);
+    }
+
+    [Fact]
+    void when_I_checkout_2_pineapples_the_system_charges_440_cents_as_there_are_no_offers_for_pineapples()
+    {
+        // var promotion = new Promotion("pear", 2, 15);
+        // var cart = new Cart(promotion);
+        var cart = new Cart();
+        
+        cart.Scan("pineapple");
+        cart.Scan("pineapple");
+
+        var total = cart.Checkout();
+        
+        Assert.Equal(440, total);
+    }
+
+    [Property]
+    Property promotions_let_the_user_save_money()
+    {
+        var useCases =
+            from promotion in Arb.Generate<Promo>()
+            from products in Arb.Generate<List<Product>>()
+            select new {Promotion=promotion, Products=products.Append(promotion.Product)};
+        
+        return Prop.ForAll(useCases.ToArbitrary(), useCase =>
+        {
+            return true;
+        });
+    }
 
 }
